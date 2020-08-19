a7fa12a71bf7dfca499e7fb2a40f1c7624f7a3c1
Mohamed Shaban <a-moshab@microsoft.com>
2020-08-19 13:29:29 +0200

    [cli tool] initialize configs.json if doesn't exist

--- CLI Tool/Configs/Models/ConfigModel.cs
+++ CLI Tool/Configs/Models/ConfigModel.cs
@@ -12,12 +12,21 @@ namespace CustomTextCliUtils.Configs.Models
 
         [JsonProperty("parser")]
         public ParserConfigModel Parser { get; set; }
+
+        public ConfigModel() {
+            Storage = new StorageConfigModel();
+            Parser = new ParserConfigModel();
+        }
     }
 
     public class ParserConfigModel
     {
         [JsonProperty("MSRead")]
         public MSReadConfigModel MsRead { get; set; }
+
+        public ParserConfigModel() {
+            MsRead = new MSReadConfigModel();
+        }
     }
 
     public class StorageConfigModel
@@ -27,5 +36,10 @@ namespace CustomTextCliUtils.Configs.Models
 
         [JsonProperty("local")]
         public LocalStorageConfigModel Local { get; set; }
+
+        public StorageConfigModel() {
+            Blob = new BlobStorageConfigModel();
+            Local = new LocalStorageConfigModel();
+        }
     }
 }
