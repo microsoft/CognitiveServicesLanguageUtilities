18995f1441506edd1a829d30010cbf195d848315
Mohamed Shaban <a-moshab@microsoft.com>
2020-08-19 10:52:40 +0200

    [cli tool] fixed configs file dir, renamed project

--- CLI Tool/MS-Read Core/CliTool/Configs/Consts/Constants.cs
+++ CLI Tool/MS-Read Core/CliTool/Configs/Consts/Constants.cs
@@ -9,7 +9,7 @@ namespace CliTool.Configs.Consts
     class Constants
     {
         public static readonly ConfigKeys ConfigKeys = new ConfigKeys();
-        public static readonly string ConfigsFileLocalDirectory = @"C:\Users\a-noyass\Desktop\LUIS\Cognitive-Custom_text_Utilities\CLI Tool\MS-Read Core\CliTool";
+        public static readonly string ConfigsFileLocalDirectory = @"";
         public static readonly string ConfigsFileName = "configs.json";
         public static readonly string[] ValidTypes = { ".pdf", ".png", ".jpg", ".jpeg", ".bmp", ".tif", ".tiff" };
 
