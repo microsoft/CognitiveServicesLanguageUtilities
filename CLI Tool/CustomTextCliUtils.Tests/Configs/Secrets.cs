42258a4584bb20b17f3063c6b77ec06126d620d8
NourEldin Yasser <noursalem95@gmail.com>
2020-09-06 10:56:09 +0200

    [PR reviews] fix Env variables names

--- CLI Tool/CustomTextCliUtils.Tests/Configs/Secrets.cs
+++ CLI Tool/CustomTextCliUtils.Tests/Configs/Secrets.cs
@@ -4,8 +4,8 @@ namespace Microsoft.CustomTextCliUtils.Tests.Configs
 {
     public class Secrets
     {
-        public static readonly string MSReadCognitiveServiceEndPoint = Environment.GetEnvironmentVariable("MSReadCognitiveServiceEndPoint");
-        public static readonly string MSReadCongnitiveServiceKey = Environment.GetEnvironmentVariable("MSReadCongnitiveServiceKey");
-        public static readonly string StorageAccountConnectionString = Environment.GetEnvironmentVariable("StorageAccountConnectionString");
+        public static readonly string MSReadCognitiveServiceEndPoint = Environment.GetEnvironmentVariable("MSREAD_COGNITIVE_SERVICES_ENDPOINT");
+        public static readonly string MSReadCongnitiveServiceKey = Environment.GetEnvironmentVariable("MSREAD_COGNITIVE_SERVICES_KEY");
+        public static readonly string StorageAccountConnectionString = Environment.GetEnvironmentVariable("STORAGE_ACCOUNT_CONNECTION_STRING");
     }
 }
