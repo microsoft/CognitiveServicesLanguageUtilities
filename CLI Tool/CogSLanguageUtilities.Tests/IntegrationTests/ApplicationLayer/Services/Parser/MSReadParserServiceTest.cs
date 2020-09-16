aa69a2b0b9893aab36f77022e71c16094c2e052a
NourEldin Yasser <noursalem95@gmail.com>
2020-09-16 10:40:38 +0200

    Revert "enable parser tests"
    This reverts commit 4c1b157a153ea5b32999301d1164bb6028ceee3a.

--- CLI Tool/CogSLanguageUtilities.Tests/IntegrationTests/ApplicationLayer/Services/Parser/MSReadParserServiceTest.cs
+++ CLI Tool/CogSLanguageUtilities.Tests/IntegrationTests/ApplicationLayer/Services/Parser/MSReadParserServiceTest.cs
@@ -50,7 +50,7 @@ namespace Microsoft.CogSLanguageUtilities.Tests.IntegrationTests.Services.Parser
             };
         }
 
-        [Theory]
+        [Theory(Skip = "Test Hangs in azure pipeline")]
         [MemberData(nameof(TestConnectionData))]
         public void TestConnection(string congnitiveServiceKey, string cognitiveServiceEndPoint, CliException expectedException)
         {
@@ -86,7 +86,7 @@ namespace Microsoft.CogSLanguageUtilities.Tests.IntegrationTests.Services.Parser
             };
         }
 
-        [Theory]
+        [Theory(Skip = "Test Hangs in azure pipeline")]
         [MemberData(nameof(TestParsingData))]
         public void TestParsing(Stream inputDocument, MSReadParserService parser, CliException expectedException)
         {
