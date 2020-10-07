c4a310b83cd32c9a84e75c37d58c022a75efab6d
Mohamed Shaban <a-moshab@microsoft.com>
2020-10-07 18:25:30 +0200

    [cli tool][docx parser] added support for parsing tables

---
+++ CLI Tool/CogSLanguageUtilities.Definitions/Enums/Parser/ElementType.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+namespace Microsoft.CogSLanguageUtilities.Definitions.Enums.Parser
+{
+    public enum ElementType
+    {
+        Root,
+        Title,
+        Heading1,
+        Heading2,
+        Heading3,
+        Paragraph,
+        BulletedList,
+        Table
+    }
+}
