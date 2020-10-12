fbe24a13c05d08d3db3ab6022dab1084afa6f05b
NourEldin Yasser <noursalem95@gmail.com>
2020-10-12 15:09:00 +0200

    Add default char-limit for chunker

--- CLI Tool/CogSLanguageUtilities.Definitions/Configs/Consts/Constants.cs
+++ CLI Tool/CogSLanguageUtilities.Definitions/Configs/Consts/Constants.cs
@@ -14,6 +14,7 @@
         public const double MaxNumberOfIndentsAfterLine = 7;
         public const double IndentPercentageOfLine = 0.05;
         public const double EndOfParagraphVerticalSpaceFactor = 1.5;
+        public const int DefaultCharLimit = 5000;
         // custom text
         public const int CustomTextPredictionMaxCharLimit = 25000;
         public const int MinAllowedCharLimit = 20;
