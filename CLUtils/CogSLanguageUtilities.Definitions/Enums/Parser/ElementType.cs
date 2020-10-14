2a868ad854e3c93dc68ebd62c1e8cd76a4f21a2b
Mohamed Shaban <a-moshab@microsoft.com>
2020-10-14 16:30:18 +0200

    [cli tool][docx parser] updated docx parser to use document tree

--- CLUtils/CogSLanguageUtilities.Definitions/Enums/Parser/ElementType.cs
+++ CLUtils/CogSLanguageUtilities.Definitions/Enums/Parser/ElementType.cs
@@ -4,13 +4,13 @@
 {
     public enum ElementType
     {
-        Root,
-        Title,
-        Heading1,
-        Heading2,
-        Heading3,
-        Paragraph,
-        BulletedList,
-        Table
+        Root = 0,
+        Title = 1,
+        Heading1 = 2,
+        Heading2 = 3,
+        Heading3 = 4,
+        Paragraph = 5,
+        BulletedList = 6,
+        Table = 7
     }
 }
