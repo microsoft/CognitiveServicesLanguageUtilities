a008d8aead6eed0e91c0143e26a77cf95d48cd19
Mohamed Shaban <a-moshab@microsoft.com>
2020-10-22 16:26:23 +0200

    [clutils][refactor] remove unwanted tracked files

--- CLI Tool/CogSLanguageUtilities.Definitions/APIs/Factories/Parser/IParserPoolManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.CogSLanguageUtilities.Definitions.APIs.Services;
-
-namespace Microsoft.CogSLanguageUtilities.Definitions.APIs.Factories.Parser
-{
-    public interface IParserPoolManager
-    {
-        public IParserService GetParser(string fileType, string fileName);
-    }
-}
