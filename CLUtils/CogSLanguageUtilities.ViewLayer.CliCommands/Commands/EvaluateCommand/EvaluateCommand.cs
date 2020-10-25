700168a726702698431715befd120d474f8a504c
Mohamed Shaban <a-moshab@microsoft.com>
2020-10-25 12:44:06 +0200

    [clutils][cli layer][help unification] added new help for evaluate command

--- CLUtils/CogSLanguageUtilities.ViewLayer.CliCommands/Commands/EvaluateCommand/EvaluateCommand.cs
+++ CLUtils/CogSLanguageUtilities.ViewLayer.CliCommands/Commands/EvaluateCommand/EvaluateCommand.cs
@@ -4,23 +4,37 @@
 using McMaster.Extensions.CommandLineUtils;
 using Microsoft.CogSLanguageUtilities.Core.Controllers;
 using Microsoft.CogSLanguageUtilities.Definitions.Models.Enums.Storage;
+using Microsoft.CogSLanguageUtilities.ViewLayer.CliCommands.CommandHelp;
+using Microsoft.CogSLanguageUtilities.ViewLayer.CliCommands.Exceptions;
 using Microsoft.CustomTextCliUtils.Configs;
 using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 
 namespace Microsoft.CogSLanguageUtilities.ViewLayer.CliCommands.Commands.EvaluateCommand
 {
-    [Command("evaluate", Description = "Evaluate trained model against labeled data")]
+    [Command("evaluate")]
     public class EvaluateCommand
     {
-        [Option("--source <local/blob>", Description = "[required] indicates source storage type")]
+        [Option("--source <local/blob>")]
         public StorageType Source { get; } = StorageType.Blob;
-        [Required]
-        [Option("--destination <local/blob>", Description = "[required] indicates destination storage type")]
+        [Option("--destination <local/blob>")]
         public StorageType Destination { get; }
+        [Option("--help")]
+        public bool Help { get; } = false;
 
         private async Task OnExecute(CommandLineApplication app)
         {
+            // show help if requested
+            if (CheckHelpRequested())
+            {
+                app.HelpTextGenerator = new CustomHelpTextGenerator();
+                app.ShowHelp();
+                return;
+            }
+
+            // validate required args
+            ValidateArguments();
+
             // build dependencies
             var container = DependencyInjectionController.BuildEvaluateCommandDependencies();
 
@@ -29,6 +43,26 @@ namespace Microsoft.CogSLanguageUtilities.ViewLayer.CliCommands.Commands.Evaluat
             {
                 var controller = scope.Resolve<BatchTestingController>();
                 await controller.EvaluateCustomTextAppAsync(Source, Destination);
+            }
+        }
+
+        private bool CheckHelpRequested()
+        {
+            // in case no arguments passed or --help specified
+            // but according to Demorgan laws, --help won't matter (noOptions && noHelp) || (noOptions && help)
+            return Destination == StorageType.NotSpecified;
+        }
+
+        private void ValidateArguments()
+        {
+            if (Help == true)
+            {
+                throw new InvalidArgumentException("--help");
+            }
+
+            if (Destination == StorageType.NotSpecified)
+            {
+                throw new RequiredArgumentException("--destination");
             }
         }
     }
