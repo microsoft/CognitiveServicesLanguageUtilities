13fb761e2349124561671022b3c74ce11549cbc1
Ahmed Leithy <v-aleithy@microsoft.com>
2021-08-04 17:31:31 +0200

    Revert "Normalized Scores to be from 0 -> 1"
    This reverts commit f5c6e596663cb0807172e7679b1c551153e92169.

--- FuzzyMatching/FuzzyMatching.Core/Services/RuntimeClient.cs
+++ FuzzyMatching/FuzzyMatching.Core/Services/RuntimeClient.cs
@@ -1,7 +1,6 @@
 ﻿using FuzzyMatching.Core.Utilities.FeatureMatrixArithmetics;
 using FuzzyMatching.Core.Utilities.MatrixArithmetics;
 using FuzzyMatching.Core.Utilities.MatrixOperations;
-using FuzzyMatching.Core.Utilities.Normalizers;
 using FuzzyMatching.Definitions.Models;
 using FuzzyMatching.Definitions.Services;
 using System.Collections.Generic;
@@ -35,14 +34,11 @@ namespace FuzzyMatching.Core.Services
             float minValue = similarityValues.Min();
             int minIndex = similarityValues.ToList().IndexOf(minValue);
 
-            // normalize cosine similarity to 0 - 1 score
-            float normalizedScore = Normalizer.NormalizeSimilarity(minValue);
-
             // return
             return new MatchingResult
             {
                 MatchingIndex = minIndex,
-                MatchingScore = normalizedScore,
+                MatchingScore = minValue,
                 ClosestSentence = Dataset[minIndex]
             };
         }
