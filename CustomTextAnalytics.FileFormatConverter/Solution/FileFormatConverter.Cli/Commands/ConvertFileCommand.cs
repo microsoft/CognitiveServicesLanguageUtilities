578aed1e64e62d965519e4878e83cc75732aaaa2
AFRICA\mshaban <mshaban@microsoft.com>
2022-01-31 22:43:51 +0200

    add cli layer

---
+++ CustomTextAnalytics.FileFormatConverter/Solution/FileFormatConverter.Cli/Commands/ConvertFileCommand.cs
@@ -0,0 +1,23 @@
+﻿using FileFormatConverter.Orchestrators;
+using McMaster.Extensions.CommandLineUtils;
+using System.ComponentModel.DataAnnotations;
+
+namespace Microsoft.CognitiveSearchIntegration.Cli.Commands
+{
+    [Command("convert", Description = "")]
+    public class ConvertFileCommand
+    {
+        [Option("--source <absolute_path>", Description = "source file path")]
+        [Required]
+        public string SourcePath { get; }
+        [Option("--target <absolute_path>", Description = "target file path")]
+        public string TargetPath { get; }
+
+        public void OnExecute(CommandLineApplication app)
+        {
+            var inputFilePath = @"C:\Users\mshaban\Desktop\cli tool\file samples\source-labeled_datapoints.jsonl";
+            var targetFilePath = @"C:\Users\mshaban\Desktop\cli tool\file samples\test.json";
+            FileConversionOrchestrator.ConvertModelFile(inputFilePath, targetFilePath);
+        }
+    }
+}
---
+++ CustomTextAnalytics.FileFormatConverter/Solution/FileFormatConverter.Cli/Start.cs
@@ -0,0 +1,21 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using Microsoft.CognitiveSearchIntegration.Cli.Commands;
+using System.Reflection;
+
+namespace Microsoft.CognitiveSearchIntegration.Cli
+{
+    [Command("")]
+    [VersionOptionFromMember("--version", MemberName = nameof(GetVersion))]
+    [Subcommand(
+        typeof(ConvertFileCommand))]
+    class Start
+    {
+        public static void Main(string[] args)
+        {
+            CommandLineApplication.Execute<Start>(args);
+        }
+
+        private static string GetVersion()
+           => typeof(Start).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+    }
+}
