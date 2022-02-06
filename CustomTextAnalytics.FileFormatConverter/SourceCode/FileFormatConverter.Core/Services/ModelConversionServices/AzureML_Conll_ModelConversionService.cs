37604f9a307854b21a6cef773a0e6699e865189e
AFRICA\mshaban <mshaban@microsoft.com>
2022-02-06 23:29:10 +0200

    tmp commit

--- CustomTextAnalytics.FileFormatConverter/SourceCode/FileFormatConverter.Core/Services/ModelConversionServices/AzureML_Conll_ModelConversionService.cs
+++ CustomTextAnalytics.FileFormatConverter/SourceCode/FileFormatConverter.Core/Services/ModelConversionServices/AzureML_Conll_ModelConversionService.cs
@@ -2,6 +2,8 @@
 using FileFormatConverter.Core.DataStructures.FileModels.IntermediateEntitiesModel;
 using FileFormatConverter.Core.Interfaces.Services;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FileFormatConverter.Core.Services.ModelConversionServices
 {
@@ -14,7 +16,38 @@ namespace FileFormatConverter.Core.Services.ModelConversionServices
 
         public IntermediateEntitiesModel ConvertToIntermediate(AzureML_Conll_FileModel sourceModel)
         {
-            throw new NotImplementedException();
+            // extract entity names
+            var entities = ExtractEntityNames(sourceModel);
+
+            // extract documents
+            var document = new EntityDocument()
+            {
+                Culture = null,
+                Location = null,
+                Entities = null
+            };
+
+            return new IntermediateEntitiesModel()
+            {
+                EntityNames = entities.ToArray(),
+                Documents = new EntityDocument[]
+                {
+                    document
+                }
+            };
+        }
+
+        private HashSet<string> ExtractEntityNames(AzureML_Conll_FileModel sourceModel)
+        {
+            var entities = new HashSet<string>();
+            sourceModel.Tokens.ToList().ForEach(token =>
+            {
+                if (token.Label != null)
+                { 
+                    entities.Add(token.Label.Text);
+                }
+            });
+            return entities;
         }
     }
 }
